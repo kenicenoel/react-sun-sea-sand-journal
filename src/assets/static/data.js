1ab5c2fe25a1c4228dc75e3dea99928e9e876cf1
Kenice Noel <kenicenoel@outlook.com>
2022-01-23 20:16:12 -0400

    Fixed typo

--- src/assets/static/data.js
+++ src/assets/static/data.js
@@ -6,7 +6,7 @@ export default [
 		startDate: "12 Jan, 2022",
 		endDate: "24 Jan, 2022",
 		description:
-			"Grand Anse Beach is one of the most popular beaches in Grenada. It is the longest white sand beach in the world and has topped the best global beaches in many charts. Grand Anse Beach is easily on the most popular tourist sites in Grenada, for both locals and tourists.",
+			"Grand Anse Beach is one of the most popular beaches in Grenada. It is the longest white sand beach in the world and has topped the best global beaches in many charts. Grand Anse Beach is easily one of the most popular tourist sites in Grenada, for both locals and tourists.",
 		imageUrl: "grand_anse_beach.jpg",
 	},
 	{
@@ -16,7 +16,7 @@ export default [
 		startDate: "12 Jan, 2022",
 		endDate: "24 Jan, 2022",
 		description:
-			"Grand Anse Beach is one of the most popular beaches in Grenada. It is the longest white sand beach in the world and has topped the best global beaches in many charts. Grand Anse Beach is easily on the most popular tourist sites in Grenada, for both locals and tourists.",
+			"Grand Anse Beach is one of the most popular beaches in Grenada. It is the longest white sand beach in the world and has topped the best global beaches in many charts. Grand Anse Beach is easily one of the most popular tourist sites in Grenada, for both locals and tourists.",
 		imageUrl: "grand_anse_beach.jpg",
 	},
 	{
@@ -26,7 +26,7 @@ export default [
 		startDate: "12 Jan, 2022",
 		endDate: "24 Jan, 2022",
 		description:
-			"Grand Anse Beach is one of the most popular beaches in Grenada. It is the longest white sand beach in the world and has topped the best global beaches in many charts. Grand Anse Beach is easily on the most popular tourist sites in Grenada, for both locals and tourists.",
+			"Grand Anse Beach is one of the most popular beaches in Grenada. It is the longest white sand beach in the world and has topped the best global beaches in many charts. Grand Anse Beach is easily one of the most popular tourist sites in Grenada, for both locals and tourists.",
 		imageUrl: "grand_anse_beach.jpg",
 	},
 	{
@@ -36,7 +36,7 @@ export default [
 		startDate: "12 Jan, 2022",
 		endDate: "24 Jan, 2022",
 		description:
-			"Grand Anse Beach is one of the most popular beaches in Grenada. It is the longest white sand beach in the world and has topped the best global beaches in many charts. Grand Anse Beach is easily on the most popular tourist sites in Grenada, for both locals and tourists.",
+			"Grand Anse Beach is one of the most popular beaches in Grenada. It is the longest white sand beach in the world and has topped the best global beaches in many charts. Grand Anse Beach is easily one of the most popular tourist sites in Grenada, for both locals and tourists.",
 		imageUrl: "grand_anse_beach.jpg",
 	},
 ];
